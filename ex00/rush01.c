2965b1f2ed9153f4149c3a72ed7e06ddbbc5cd1d
josekgobJHB <josekgob@student.wethinkcode.co.za>
2020-07-15 15:02:09 +0200

    rush01

--- ex00/rush01.c
+++ ex00/rush01.c
@@ -6,12 +6,15 @@
 /*   By: josekgob <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/15 08:53:07 by josekgob          #+#    #+#             */
-/*   Updated: 2020/07/15 14:58:20 by josekgob         ###   ########.fr       */
+/*   Updated: 2020/07/15 15:01:02 by josekgob         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-void ft_putchar(char    c);
-void rush00(int     x, int      y)
+#include	<unistd.h>
+void ft_putchar(char    c)
+{
+				write(1, &c, 1);
+}
+void rush01(int     x, int      y)
 {
                 int 	counter;
                 int 	num_columns1;
@@ -22,37 +25,42 @@ void rush00(int     x, int      y)
 				num_columns2 = x;
                 if(y > 0 && x > 0)
                 {
-                    ft_putchar('o');
+                    ft_putchar('/');
                     
                     if(x > 1)
                     {
                     while(x > 1)
                     {
-                                    ft_putchar('-');
+                                    ft_putchar('*');
                                     x--;
                     }
-                    ft_putchar('o');
+                    ft_putchar('\\');
                     }
                     while(y > 1)
                     {           ft_putchar('\n');
-                                ft_putchar('|');
+                                ft_putchar('*');
                                 while(num_columns1 > 1)
                                 {
                                                 ft_putchar(' ');
 												num_columns1--;
                                 }
-                                ft_putchar('|');
+                                ft_putchar('*');
                                 y--;
 								num_columns1 = num_columns2;
                     }
                     ft_putchar('\n');
-                    ft_putchar('o');
+                    ft_putchar('\\');
                     while(num_columns2 > 1)
                     {
-                                    ft_putchar('-');
+                                    ft_putchar('*');
                                     num_columns2--;
                     }
-                    ft_putchar('o');
+                    ft_putchar('/');
                 }
                 ft_putchar('\n');
+}
+int		main(void)
+{
+				rush01(5, 5);
+				return	(0);
 }
